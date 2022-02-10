08b47d89a29dcbe237f3f434cec65a8ae205131a
Gerhard_Steenkamp <gerhard@linumlabs.com>
2022-02-10 09:50:17 +0200

    remove weird exportds

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,3 @@
 export * from './config/config';
 export * from './config/wallets';
 export * from './utils';
-export * from '@usedapp/core';
-export * from '@ethersproject/units';
-export * from '@web3-react/abstract-connector';
