e96920e11d5eafde9feb2c1114ffe08c41c617f5
Gerhard_Steenkamp <gerhard@linumlabs.com>
2022-02-10 15:28:29 +0200

    update example

--- example/index.tsx
+++ example/index.tsx
@@ -1,7 +1,7 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { L2Provider } from '../.';
+import { L2Provider } from '../dist';
 import SomeComponent from './components/SomeComponent';
 
 const App = () => {
