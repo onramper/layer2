104d9841aa32e9bec1e1dfbaa7688274a4c4632a
Gerhard_Steenkamp <gerhard@linumlabs.com>
2022-02-11 11:16:18 +0200

    add package to preventAssignment (prob not necessary)

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,6 @@
 export * from './config/config';
 export * from './config/wallets';
 export * from './utils';
+export * from '@usedapp/core';
+export * from '@ethersproject/units';
+export * from '@web3-react/injected-connector';
