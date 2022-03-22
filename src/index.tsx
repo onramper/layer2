e2c3281d422cb3652d90d57bd6574ebe0086ecd3
Gerhard_Steenkamp <gerhard@linumlabs.com>
2022-03-22 13:17:43 +0200

    HOTFIX: export ethers bignumber package

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,5 @@ export * from './tokens';
 export * from '@usedapp/core';
 export * from '@ethersproject/units';
 export * from '@ethersproject/address';
+export * from '@ethersproject/bignumber';
 export * from '@web3-react/injected-connector';
