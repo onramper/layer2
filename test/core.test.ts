ffcea49330bd3e1f225a14e73f9a06a5d6d2adf3
Gerhard_Steenkamp <gerhard@linumlabs.com>
2022-04-11 19:54:02 +0200

    synchronous core functions

--- test/core.test.ts
+++ test/core.test.ts
@@ -1,18 +1,22 @@
-// import * as React from 'react';
-// import * as ReactDOM from 'react-dom';
-// import { L2Provider } from '../src';
+import { blockExplorerAddressLink, blockExplorerTransactionLink } from '../src';
+const CHAIN_ID = 1;
+const USER_WALLET = '0xC54070dA79E7E3e2c95D3a91fe98A42000e65a48';
+const TX_HASH =
+  '0x4ae403ad01bee9a3192cede2d2ac2e398775ca7b145af8cad86a139fd507fee8';
 
-describe('just passing', () => {
-  it('this shouldfail', () => {
-    expect(true).toBe(true);
+// SYNCHRONOUS FUNCTIONS
+describe('blockExplorerAddressLink', () => {
+  it('returns a usable link from chainID and address', () => {
+    const link = blockExplorerAddressLink(CHAIN_ID, USER_WALLET);
+    expect(typeof link).toBe('string');
   });
 });
 
-// const div = document.createElement('div');
-//     ReactDOM.render(
-//       <L2Provider>
-//         <div>hello</div>
-//       </L2Provider>,
-//       div
-//     );
-//     ReactDOM.unmountComponentAtNode(div);/
+describe('blockExplorerTransactionLink', () => {
+  it('returns a usable link from chainID and transaction hash', () => {
+    const link = blockExplorerTransactionLink(CHAIN_ID, TX_HASH);
+    expect(typeof link).toBe('string');
+  });
+});
+
+// ASYNCHRONOUS FUNCTIONS
