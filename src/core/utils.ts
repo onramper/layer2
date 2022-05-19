06a3cbe2bb47648273d040c8ccc112136fde361a
Gerhard_Steenkamp <gerhard@linumlabs.com>
2022-05-19 18:04:16 +0200

    with major usedapp version

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,7 +1,7 @@
 import { MetaMaskProvider, QuoteDetails, RouteDetails } from './models';
 import { TokenInfo } from '../tokens';
 import { NativeCurrencies } from './constants';
-import { utils, BigNumber } from 'ethers';
+import { utils, BigNumber, providers } from 'ethers';
 
 export const isNativeToken = (token: TokenInfo): boolean => {
   const wethResolved = resolveWeth(token);
@@ -35,7 +35,7 @@ export const formatTokenAmount = (
 
 // returns "0x" padded hex of chain ID
 export const toHex = (decimal: number): string => {
-  return '0x' + decimal.toString(16);
+  return `0x${decimal.toString(16)}`;
 };
 
 export const isMetamaskEnabled = (): boolean => {
@@ -80,11 +80,17 @@ export const uriToHttp = (uri: string): string[] => {
   }
 };
 
-export const getAddressFromEnsName = (ensName: string, library: any) => {
+export const getAddressFromEnsName = (
+  ensName: string,
+  library: providers.JsonRpcProvider
+) => {
   return library.resolveName(ensName);
 };
 
-export const getEnsNameFromAddress = (address: string, library: any) => {
+export const getEnsNameFromAddress = (
+  address: string,
+  library: providers.JsonRpcProvider
+) => {
   if (utils.isAddress(address)) {
     return library.lookupAddress(address);
   } else return null;
